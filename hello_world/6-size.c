512006188ba8089f9af5adf0595ab89f0987eaa1
Nitsu <agustraversa2004@gmail.com>
2023-06-15 06:01:15 -0700

    Typing fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,10 +8,10 @@ float floatType;
 char charType;
 long long int longlongintType;
 long int longintType;
-printf("char is %zu byte(s)\n", sizeof(charType));
-printf("int is %zu byte(s)\n", sizeof(intType));
-printf("long int is %zu byte(s)\n", sizeof(longintType));
-printf("long long int is %zu byte(s)\n", sizeof(longlongintType));
-printf("float is %zu byte(s)\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
