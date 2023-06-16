adf912db87977c6589cdb320d544fcb386654712
Nitsu <agustraversa2004@gmail.com>
2023-06-16 12:41:22 -0700

    program that prints _putchar, followed by a new line

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+int
+main(void)
+{
+char frase[] = "_putchar";
+int x;
+for (x = 0; x <= 7; x++)
+_putchar (frase[x]);
+putchar('\n');
+return (0);
+}
