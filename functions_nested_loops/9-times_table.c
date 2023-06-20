371799ecf1a726c01fe7c9d4a684d7c50a7dcc35
Nitsu <agustraversa2004@gmail.com>
2023-06-20 11:09:55 -0700

    Script fix

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,29 +1,35 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 void
 times_table(void)
 {
-int x, y;
+int x, y, z;
 
-for (x = 0; x < 10; x++)
+for (x = 0; x <= 9; x++)
 {
-for (y = 0; x < 10; y++)
-{
-int z = x * y;
-if ((x / 10) == 0)
-{
-putchar (' ');
-}
-putchar ((z / 10) + '0');
-putchar ((z % 10) + '0');
-if (x == 9)
-{
-break;
-}
-putchar (',');
-putchar (' ');
-}
-putchar ('\n');
-}
+	_putchar('0');
+	_putchar(44);
+	_putchar(32);
+	for (y = 1; y <= 9; y++)
+	{
+	z = (x * y);
+		if ((z / 10) > 0)
+		{
+		_putchar((z / 10) + '0');
+		}
+		else
+		{
+		_putchar(32);
+		}
+	_putchar((z % 10) + '0');
+
+	if (y < 9)
+	{
+	_putchar(44);
+	_putchar(32);
+	}
+	}
+	_putchar('\n');
+	}
 }
