89e16adc9c4e722597535b163ba82f27c4fd2fb3
Nitsu <agustraversa2004@gmail.com>
2023-06-20 10:34:43 -0700

    4

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int _putchar(char c);
+int _putchar(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -9,3 +9,5 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
