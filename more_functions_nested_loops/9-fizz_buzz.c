d74d5664e0ce2e7cac1258f44d6f31596cca2806
Nitsu <agustraversa2004@gmail.com>
2023-06-20 19:18:16 -0700

    Script fixed

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include "main.h"
 
 int
 main(void)
 {
-int x = 1;
+	int x = 1;
 
-while (x <= 100)
+	while (x <= 100)
 	{
-	if (x % 3 == 0 && x % 5 == 0)
+		if (x % 3 == 0 && x % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
