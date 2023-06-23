ece6416fb6634492cf7bf20274461e0b302b8c6b
Nitsu <agustraversa2004@gmail.com>
2023-06-23 06:05:57 -0700

    Syntax error fixed, now prints in reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_rev - check the code
+ * print_rev -check the code
  * @s: pointer
- * Return: nothing.
+ * return nothing
  */
 
 void print_rev(char *s)
 {
+int a = 0;
 
+while (s[a] != '\0')
+{
+a++;
+}
+while (a)
+{
+_putchar(s[--a]);
+}
+_putchar ('\n');
 }
