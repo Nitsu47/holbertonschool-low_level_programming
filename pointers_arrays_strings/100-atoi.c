ae756d771dfdf063c7663efec109f3e27be6f3f3
Nitsu <agustraversa2004@gmail.com>
2023-06-26 12:59:39 -0700

    100-atoi added to continue later

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - convert a string to a integer
+ * @s: pointer
+ * Return: Always 0.
+ */
+
+int _atoi(char *s)
+{
+int x = 0;
+int y = 0;
+int z = 1;
+
+	while (s[x])
+	{
+		if (x == 45)
+		{
+			z = z * -1;
+		}
+		if	(s[x] <= 47 && s[x] >= 58)
+		{
+			
+		}
+		if ()
+	}
+	return (0);
+}
