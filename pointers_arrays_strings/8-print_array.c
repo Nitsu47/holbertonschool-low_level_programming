3d029bd933afd1941a58081330dfad23d8c52eb3
Nitsu <agustraversa2004@gmail.com>
2023-06-26 08:58:21 -0700

    Typing errors fixed

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,9 @@
 
 void print_array(int *a, int n)
 {
-int x = 0;
+int x;
 
-	for (x = 0; x <= n; x++)
+	for (x = 0; x < n; x++)
 	{
 		printf("%d", a[x]);
 
@@ -21,5 +21,5 @@ int x = 0;
 
 		printf(", ");
 	}
-_putchar('\n');
+	printf("\n");
 }
