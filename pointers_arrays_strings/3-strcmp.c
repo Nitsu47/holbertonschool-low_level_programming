b8c76d66bc8829967c0f868616e8c656adce732c
Nitsu <agustraversa2004@gmail.com>
2023-06-27 11:52:36 -0700

    Script totally reworked and simplified

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,24 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int la;
-int lb;
-
-	for (la = 0; s1[la] != '\0'; la++)
+	while (*s1 && *s2)
 	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
 	}
-	for (lb = 0; s2[lb] != '\0'; lb++)
-	{
-	}
-	if (s1[la] > s2[lb])
-	{
-		return (la);
-
-	if (s1[la] < s2[lb])
-	{
-		return (la *= -1);
-	}
-	}
-	else
 	return (0);
 }
