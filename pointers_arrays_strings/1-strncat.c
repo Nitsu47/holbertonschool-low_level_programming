00878d356d407b6f526ab82d3269f29c7254893f
Nitsu <agustraversa2004@gmail.com>
2023-06-26 20:42:31 -0700

    main and task 1 to do later

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: pointer
+ * @src: pointer
+ * @n: variable
+ * Return: strings.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+}
