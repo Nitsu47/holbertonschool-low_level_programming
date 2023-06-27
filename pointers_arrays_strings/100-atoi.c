2a4d4b21638e81b47b932a4742a35a316a219e72
Nitsu <agustraversa2004@gmail.com>
2023-06-26 19:50:40 -0700

    * added to z = -1

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,27 +9,30 @@
 
 int _atoi(char *s)
 {
-int x;
-int y = 0;
-int z = -1;
+	int x;
+	int y, z;
 
+	y = 0;
+	z = -1;
 	for (x = 0; s[x] != '\0'; x++)
 	{
-		if (s[x] == 45)
-		{
+		if (s[x] == '-')
 			z *= -1;
-		if	(s[x] > 47 && s[x] < 58)
+
+		if (s[x] > 47 && s[x] < 58)
 		{
 			if (y < 0)
-				y = (y * 10) - (s[x] - '\0');
+
+				y = (y * 10) - (s[x] - '0');
 			else
-				y = (s[x] - '\0') * -1;
+				y = (s[x] - '0') * -1;
+
 			if (s[x + 1] < 48 || s[x + 1] > 57)
 				break;
 		}
-		}
 	}
 		if (z < 0)
-			x *= -1;
+			y *= -1;
+
 	return (y);
 }
