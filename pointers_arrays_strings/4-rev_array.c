ce3fbe006bebe50aba12f6c845302a57fc7e9994
Nitsu <agustraversa2004@gmail.com>
2023-06-27 11:59:24 -0700

    main.h, main.c and task 4 added for do later

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse the content of an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+
+void reverse_array(int *a, int n)
+{
+
+}
