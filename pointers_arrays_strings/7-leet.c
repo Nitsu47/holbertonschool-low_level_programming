c68baae2066e326c8a03a622bc076fb7e9e8aeae
Nitsu <agustraversa2004@gmail.com>
2023-06-27 20:03:43 -0700

    Typing fix

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * *leet - Encodes a string into 1337
- *   * @a: string pointer
- *    * Return: a
- *     */
+ * *leet - Encodes a string into 1337
+ * @a: string pointer
+ * Return: a
+ */
 
 char *leet(char *a)
 {
@@ -14,14 +14,14 @@ int count = 0;
 
 	while (a[count])
 	{
-			while (lett[countlet])
+		while (lett[countlet])
+		{
+			if (a[count] == lett[countlet])
 			{
-					if (a[count] == lett[countlet])
-					{
-							a[count] = num[countlet];
-					}
-					countlet++;
+				a[count] = num[countlet];
 			}
+			countlet++;
+		}
 			countlet = 0;
 			count++;
 	}
