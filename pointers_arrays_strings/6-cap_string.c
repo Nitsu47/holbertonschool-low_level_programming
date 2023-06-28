67aab25a42d15cf1da1989148358e466e1119744
Nitsu <agustraversa2004@gmail.com>
2023-06-28 06:58:59 -0700

    function reworked

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,35 +8,31 @@
 
 char *cap_string(char *a)
 {
-int up = 0;
+int up;
 
-	while (a[up])
+	for (up = 0; a[up] != '\0'; up++)
 	{
-		if (a[up] >= 97 && a[up] <= 122)
+		if (a[up] > 96 && a[up] < 123)
 		{
-			if (a[up] == 0)
+			if (a[up - 1] == ' ' ||
+					a[up - 1] == '\t' ||
+					a[up - 1] == '\n' ||
+					a[up - 1] == ',' ||
+					a[up - 1] == ';' ||
+					a[up - 1] == '.' ||
+					a[up - 1] == '!' ||
+					a[up - 1] == '?' ||
+					a[up - 1] == '{' ||
+					a[up - 1] == '}' ||
+					a[up - 1] == '(' ||
+					a[up - 1] == ')' ||
+					a[up - 1] == '"' ||
+					up == 0)
 			{
-			a[up] = a[up] - 32;
-			}
-			switch (a[up + 1])
-			{
-				case 3:
-				case 9:
-				case 10:
-				case 44:
-				case 59:
-				case 46:
-				case 33:
-				case 63:
-				case 34:
-				case 40:
-				case 41:
-				case 123:
-				case 125:
 				a[up] = a[up] - 32;
 			}
+
 		}
-		up++;
 	}
 	return (a);
 }
