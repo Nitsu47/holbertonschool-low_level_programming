53f6eb70f6342227e1cd8a9bbb65de115caa9f82
Nitsu <agustraversa2004@gmail.com>
2023-06-28 13:20:06 -0700

    Finally script added and corrupt script fixed

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - check the code
+ * @s: pointer
+ * Return: Nothing.
+ */
+
+void _print_rev_recursion(char *s)
+{
+int l = 0;
+
+	if (s[l] != '\0')
+	{
+		l++;
+	}
+	if (l)
+	{
+		_putchar(s[l--]);
+	}
+}
