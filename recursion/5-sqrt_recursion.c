d9953c63c6eab54b247c1420bb13e9027356f9e2
Nitsu <agustraversa2004@gmail.com>
2023-06-29 10:54:32 -0700

    Script reworked

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+int
+_sqrt_recursion(int n)
+{
+int y = 1;
+
+	y = aux(n, y);
+	return (y);
+}
+
+int
+aux(int n, int y)
+{
+	if (n == (y * y))
+	{
+		return (n);
+	}
+	else if (n == y || y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		y++;
+	}
+	return (aux(n, y));
+}
