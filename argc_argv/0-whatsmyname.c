cc153bc0dfc352b51425c0e5baf982059cff5984
Nitsu <agustraversa2004@gmail.com>
2023-07-02 18:53:38 -0700

    Program that prints its name followed by a new line

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
-int main (int argc, char *argv)
+int
+main(int argc, char *argv[])
 {
-	printf ("\n");
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
 }
