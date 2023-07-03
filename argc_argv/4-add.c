c8a73e93888d20622ce4668af61c55bcbba5837f
Nitsu <agustraversa2004@gmail.com>
2023-07-02 20:16:09 -0700

    changes to work later

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -5,24 +5,16 @@
 int
 main(int argc, char *argv[])
 {
-	int res = 0;
-	int count = 1;
-	int countOfCount = 0;
+	int r = 0;
+	int c = 1;
 
-	while (count < argc)
+	while (c < argc)
 	{
-			while (argv[count][countOfCount])
+			if (argv[c] < 48 || argv[c] > 59)
 			{
-					if (argv[count][countOfCount] < 48 || argv[count][countOfCount] > 59)
-					{
-						printf("Error\n");
-						return (1);
-					}
-					countOfCount++;
-					countOfCount = 0;
-					res += atoi(argv[count]);
-					count++;
-			}
-			printf("%d\n", res);
-			return (0);
-}
+				printf("Error\n");
+				return (1)
+	}
+		printf("%d\n", r);
+		return (0);
+	}
