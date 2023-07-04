a233b15167801feb25db092d78fc8fcc1d0a7279
Nitsu <agustraversa2004@gmail.com>
2023-07-04 06:43:13 -0700

    Syntax fix

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -7,14 +7,23 @@ main(int argc, char *argv[])
 {
 	int r = 0;
 	int c = 1;
+	int n = 0;
 
 	while (c < argc)
 	{
-			if (argv[c] < 48 || argv[c] > 59)
+		while (argv[c][n])
+		{
+			if (argv[c][n] < 48 || argv[c][n] > 59)
 			{
 				printf("Error\n");
-				return (1)
+				return (1);
+			}
+			n++;
+		}
+		n = 0;
+		r += atoi(argv[c]);
+		n++;
 	}
-		printf("%d\n", r);
-		return (0);
-	}
+	printf("%d\n", r);
+	return (0);
+}
