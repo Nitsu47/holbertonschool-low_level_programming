2531fb488248c1f78c133ad7ca5b623eeb44a487
Nitsu <agustraversa2004@gmail.com>
2023-07-07 06:11:54 -0700

    if strings not valid condition added

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -28,12 +28,14 @@ char *str_concat(char *s1, char *s2)
 	if (total == NULL)
 		return (NULL);
 
-	if (s1 != NULL || s2 != NULL)
-	{
+	if (!s1)
+		return (NULL);
+	if (!s2)
+		return (NULL);
+
 	memcpy(total, s1, l1 * sizeof(char));
 
 	memcpy(total + l1, s2, l2 * sizeof(char));
-	}
 
 	total[new] = '\0';
 
