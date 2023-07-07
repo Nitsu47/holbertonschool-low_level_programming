90b4747dc1e1cd61dddd33d2c0d6082499b559b2
Nitsu <agustraversa2004@gmail.com>
2023-07-07 06:24:41 -0700

    size_t declarations fixed

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,11 +11,18 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	size_t l1 = strlen(s1);
-	size_t l2 = strlen(s2);
-	size_t new = l1 + l2;
+	size_t l1;
+	size_t l2;
+	size_t new;
 
 	char *total;
+
+	if (s1)
+		l1 = strlen(s1);
+	if (s2)
+		l2 = strlen(s2);
+	if (s1 || s2)
+		new = l1 + l2;
 
 	if (s1 == NULL && s2 == NULL)
 	{
