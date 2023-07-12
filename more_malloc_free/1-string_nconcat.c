7b303283d40b3651f93a22c268c0e762b4c013f3
Nitsu <agustraversa2004@gmail.com>
2023-07-12 05:46:08 -0700

    '<' changed to '>'

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -24,17 +24,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	for (l1 = 0; s1[l1] != '\0'; l1++)
 		;
+
 	for (l2 = 0; s2[l2] != '\0' && l2 < n; l2++)
 		;
-	total = malloc((l1 + l2) * sizeof(char) + 1);
 
+	total = malloc((l1 + l2 + 1) * sizeof(char));
 	if (total == NULL)
 		return (NULL);
+
 	for (x = 0; x < l1 + l2; x++)
 	{
 		if (x < l1)
 			total[x] = s1[x];
-		if (x <= l1)
+		if (x >= l1)
 			total[x] = s2[x - l1];
 	}
 	total[l1 + l2] = '\0';
