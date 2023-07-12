15ef1f48ecdb7e4cd7cb65f22eadd74951ba74e3
Nitsu <agustraversa2004@gmail.com>
2023-07-12 14:47:33 -0700

    trailing whitespace deleted

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef three_function_like_macro_h
 #define three_function_like_macro_h
-#define ABS(x) (x > 0 ? (x) : (x) * -1) 
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
 #endif /* three_function_like_macro_h */
