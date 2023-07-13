6958c8972520f1100e5d2c1b83c6633d698ca523
Nitsu <agustraversa2004@gmail.com>
2023-07-12 17:44:46 -0700

    README and dog.h added

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - Dog´s info
+ * @name: Dog´s name
+ * @owner: Dog´s owner
+ * @age: Dog´s age
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif /* dog_h */
