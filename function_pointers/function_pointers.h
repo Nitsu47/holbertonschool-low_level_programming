f4f8eeeb7f6d411601e7f55d6d7a2c99c4dc8c67
Nitsu <agustraversa2004@gmail.com>
2023-07-13 14:06:36 -0700

    guards added to .h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
+#ifndef function_pointers_h
+#define function_pointers_h
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,4 +7,4 @@ void print_name(char *name, void (*f)(char *));
 int _putchar (char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
+#endif /* function_pointers_h */
