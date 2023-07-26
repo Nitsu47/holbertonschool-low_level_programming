a0d513be85097e17791387c1af1c481500697af9
Nitsu <agustraversa2004@gmail.com>
2023-07-26 15:18:34 -0700

    exercise 1 started

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * list_len - list number of elements
+ * @h: pointer to structure
+ * Return: number of elements in a linked list_t list.
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+
+}
