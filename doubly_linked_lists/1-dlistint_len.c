4478bf723cb828a8c6598e095fae1c4e1cb12183
Nitsu <agustraversa2004@gmail.com>
2023-07-30 18:18:57 -0700

    segmentation fault fix

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 /**
  * dlistint_len - return the number of elements in a linked list.
- * @h: pointer to the head of the list.
- * Return: number of elements.
+ * @h: pointer to the list.
+ * Return: number of nodes.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t numbofe = 0;
 
-	while (!h)
+	while (h != NULL)
 	{
 		h = h->next;
 		numbofe++;
