c5d55da68a45c048864d607e11c6c0159779623a
Nitsu47 <agustraversa2004@gmail.com>
2023-09-19 19:47:17 +0000

    tabs fixes

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -8,26 +8,26 @@
 */
 int binary_search(int *array, size_t size, int value)
 {
-    int low = 0, high = size - 1, mid, ite;
+	int low = 0, high = size - 1, mid, ite;
 
-    if (array == NULL || size < 1)
-        return (-1);
-    
-    while (low <= high)
-    {
-        printf("Searching in array: ");
-        for (ite = low; ite < high; ite++)
-            printf("%d ", array[ite]);
-        if (ite == high)
-            printf("%d", array[ite]);
-        printf("\n");
-        mid = low + (high - low) / 2;
-        if (array[mid] < value)
-            low = mid + 1;
-        if (array[mid > value])
-            high = mid - 1;
-        if (array[mid] == value)
-            return (mid);
-    }
-    return (-1);
+	if (array == NULL || size < 1)
+		return (-1);
+	
+	while (low <= high)
+	{
+		printf("Searching in array: ");
+		for (ite = low; ite < high; ite++)
+			printf("%d, ", array[ite]);
+		if (ite == high)
+			printf("%d", array[ite]);
+		printf("\n");
+		mid = low + (high - low) / 2;
+		if (array[mid] < value)
+			low = mid + 1;
+		if (array[mid > value])
+			high = mid - 1;
+		if (array[mid] == value)
+			return (mid);
+	}
+	return (-1);
 }
