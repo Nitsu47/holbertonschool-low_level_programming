a17736a3c5d06074185d255a0b3cccd91345ce5f
Nitsu47 <agustraversa2004@gmail.com>
2023-09-19 15:28:45 +0000

    spaces converted in tabs for betty codestyle

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -4,20 +4,20 @@
 * @array: array of integers.
 * @size: size of the array.
 * @value: value to find in the array.
-* Return: 
+* Return: First index with the value.
 */
 int linear_search(int *array, size_t size, int value)
 {
-    unsigned int ite;
+	unsigned int ite;
 
-    if (array ==  NULL)
-        return (-1);
+	if (array ==  NULL)
+		return (-1);
 
-    for (ite = 0; ite < size; ite++)
-    {
-        printf("Value checked array[%d] = [%d]\n", ite, array[ite]);
-        if (array[ite] == value)
-            return (ite);
-    }
-    return (-1);
+	for (ite = 0; ite < size; ite++)
+	{
+		printf("Value checked array[%d] = [%d]\n", ite, array[ite]);
+		if (array[ite] == value)
+			return (ite);
+	}
+	return (-1);
 }
