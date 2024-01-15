a54dc1178725f8ac0918e36ecb0cbd3ee7ffdb0c
Nitsu47 <agustraversa2004@gmail.com>
2024-01-15 00:49:30 +0000

    bettystyle fix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "search_algos.h"
 /**
  * linear_search - linear search algorithm, starts from the beginning,
  * and doesn't stop until the value is found.
@@ -10,21 +9,18 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    unsigned int i;
+	unsigned int i;
 
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    for (i = 0; i < size; i++)
-    {
+	for (i = 0; i < size; i++)
+	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 
-        if (array[i]== value)
-        {
-            
-            return (i);
-        }
+		if (array[i]== value)
+			return (i);
     }
 
-    return (-1);
+	return (-1);
 }
